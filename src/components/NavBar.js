bc77c7d389fbf4bf74577a2aed3e3c2e31e99187
serkansahin55 <ssahin55@outlook.com>
2021-01-12 03:50:02 -0500

    commit tree cfb51ea75b3e4017c3e74221613ac24a689b3bc9

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ["home", "about", "projects"]
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  const linksList = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+    ))
+    return <nav>{linksList}</nav>
 
+  }
+  
 export default NavBar;
+
+
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+
+//   const anchors = links.map((link) => (
+//     <a key={link} href={"#" + link}>
+//       {link}
+//     </a>
+//   ));
+//   return <nav>{anchors}</nav>;
+// }
